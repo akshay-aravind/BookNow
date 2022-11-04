35af04ea6f37357b1c17d3027426942b4cddbe4d
akshay-aravind <akshay.dx4@gmail.com>
2022-11-04 23:12:06 +0530

    inwork

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,26 @@
 import express from 'express'
+import { deleteUser, getUser, getUsers, updateUser } from '../controllers/user.js'
+import { verifyToken } from '../utils/verifyToken.js'
 
 const router = express.Router()
 
+router.get("/checkauthentication", verifyToken, (req,res,next)=> {
+    res.send("hello User,You are Logged In")
+})
 
+//UPDATE
+router.put('/:id', updateUser)
 
-export default router
+//DELETE
+
+router.delete('/:id', deleteUser)
+
+//GET User
+
+router.get('/:id', getUser)
+
+//GET all Users
+
+router.get('/', getUsers)
+
+export default router  
